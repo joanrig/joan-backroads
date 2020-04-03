889f35c09bfe98e9a354beaef83917cc1af05762
@joanrig <joan.lyness@vistaprint.com>
2020-04-03 19:45:12 -0400

    abstracted header.js query into a constant

--- src/examples/Header.js
+++ src/examples/Header.js
@@ -1,28 +1,30 @@
 import React from 'react'
 import { useStaticQuery, graphql } from 'gatsby'
 
-const Header = () => {
-
-    const { site:{siteMetadata: {data}} } = useStaticQuery(graphql`
-    {
-        site {
-          siteMetadata {
-            title
-            description
-            author
-            data {
-              name
-              age
-            }
-          }
+const getSiteData = graphql`
+{
+    site {
+      siteMetadata {
+        title
+        description
+        author
+        data {
+          name
+          age
         }
       }
-    `)
+    }
+  }
+`
+
+const Header = () => {
+
+    const { site:{siteMetadata} } = useStaticQuery(getSiteData)
     console.log(siteMetadata)
     return (
         <div>
             <h1>title:{siteMetadata.title}</h1>
-            <h1>author:</h1>           
+            <h1>author:{siteMetadata.author}</h1>           
         </div>
     )
 }
