da22eafe5f70ed278d7ae4b8917b1901d04495d6
@joanrig <joan.lyness@vistaprint.com>
2020-04-06 15:53:02 -0400

    render images

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import Layout from '../components/Layout'
+import Images from '../examples/Images'
 
 
 const blog = () => {
     return (
         <Layout>
-            <h1>hello from blog page</h1>
-            
-            <Link to="/">Back home</Link>
+            <Images/>
         </Layout>
     )
 }
