2063204a5a8fdaef0a1630aea6ea198cd0c71c5a
@joanrig <joan.lyness@vistaprint.com>
2020-04-06 16:48:31 -0400

    added traced svg to about image

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import Layout from '../components/Layout'
-import Images from '../examples/Images'
+
 
 
 const blog = () => {
     return (
         <Layout>
-            <Images/>
+            
         </Layout>
     )
 }
