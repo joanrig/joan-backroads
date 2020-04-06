56c58a86bd923307aa44237dddb18f1ca0928421
@joanrig <joan.lyness@vistaprint.com>
2020-04-06 15:52:52 -0400

    add more images

---
+++ src/examples/Images.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import { graphql, useStaticQuery } from 'gatsby'
+import styled from 'styled-components'
+import img from '../images/connectBcg.jpeg'
+import Img from 'gatsby-image'
+
+const getImages = graphql`
+query Images {
+    fixed: file(relativePath: {eq: "defaultBcg.jpeg"}) {
+       childImageSharp {
+      fixed(width:300, height:400){
+        ...GatsbyImageSharpFixed
+      }
+     }
+    }
+    fluid: file(relativePath: {eq: "blogBcg.jpeg"}){
+      childImageSharp{
+        fluid{
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    
+  } 
+`
+
+const Images = () => {
+    const data = useStaticQuery(getImages)
+    console.log(data)
+    return <Wrapper>
+       <article>
+           <h3>basic image</h3>
+           <img src={img} className="basic"/>
+        </article>
+       
+       <article>
+           <h3>fixed image/ blur</h3>
+           <Img fixed={data.fixed.childImageSharp.fixed}/>
+       </article>
+        
+       <article>
+           <h3>fluid image/ svg</h3>
+       </article>
+   </Wrapper>
+}
+
+const Wrapper = styled.div`
+    text-align: center;
+    text-transform:capitalize;
+    width: 80vw;
+    margin:0 auto 10rem auto;
+    article{
+        border:3px solid red;
+        padding:1rem 0;
+    }
+    .basic{
+        width: 100%;
+    }
+
+`
+
+export default Images
