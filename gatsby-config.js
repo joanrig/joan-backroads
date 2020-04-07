53fb54faaa025141dc2f9722a05cbfaa6fe02773
@joanrig <joan.lyness@vistaprint.com>
2020-04-07 11:31:52 -0400

    added contentful and env variables

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,6 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 
 module.exports = {
   /* Your site config here */
@@ -21,10 +19,21 @@ module.exports = {
       options: {
         name: `images`,
         path: `${__dirname}/src/images/`,
-      }
+      },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: 
+        process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
     },
     `gatsby-plugin-sass`, 
     `gatsby-plugin-styled-components`,
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`],
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-transition-link`
+  ],
 }
