b9227f16aa4b97f17e87cf7a31d54f135a682e80
@joanrig <joan.lyness@vistaprint.com>
2020-04-07 16:00:57 -0400

    fixed linear gradient by adding 2nd rgb value

--- src/components/StyledHero.js
+++ src/components/StyledHero.js
@@ -19,10 +19,11 @@ export default styled(StyledHero)`
         return props.home ? 'calc(100vh - 62px)' : '50vh'
         }
     };
-    background:${props => {
-        return props.home ? 'linear-gradient(rgba(63, 208, 212, 0.7)' : 'none'
-        }
-    };
+    background: ${props => {
+        return props.home
+        ? "linear-gradient(rgba(63, 208, 212, 0.7), rgba(0, 0, 0, 0.7))"
+        : "none"
+    }};
     background-position: center;
     background-size: cover;
     opacity: 1 !important;
